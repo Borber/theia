2f2db5187f537dbf2ed836e95c6594d4f4537ff2
BORBER <Borber.Drum@gmail.com>
2019-08-31 11:28:38 +0000

    up

--- Java/HelloWorld.java
+++ Java/HelloWorld.java
@@ -1,10 +1,22 @@
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * HelloWorld
  */
 
 public class HelloWorld {
+    public static int f(int n) {
+        if (n == 0) {
+            return 0;
+        } else
+            return 2 * f(n - 1) + n * n;
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Random a = new Random();
+        for(int i=0; i < 100; ++i){
+            System.out.println(a.nextInt(10));
+        }
     }
 }
