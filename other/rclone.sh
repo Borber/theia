9a3ef5d94b6cb8846662fbad8d7c6911fb6b037f
BORBER <Borber.Drum@gmail.com>
2019-09-01 08:10:55 +0000

    fix rclone

--- other/rclone.sh
+++ other/rclone.sh
@@ -1,4 +1,4 @@
 wget https://www.moerats.com/usr/shell/rclone_debian.sh && bash rclone_debian.sh
+# 2
+wget https://rclone.org/install.sh && bash install.sh
 rclone config
-mkdir /workspace/ONE
-rclone mount BORBER_ONE:BORBER /workspace/ONE --copy-links --no-gzip-encoding --no-check-certificate --allow-other --allow-non-empty --umask 000
